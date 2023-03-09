f8afec79a114edcbaa17274d794bd94488af1f2e
nadilsandaruwan97@gmail.com <87482209+Nadil97@users.noreply.github.com>
2023-03-09 12:35:53 +0530

    create firebase account and connect

---
+++ lib/login_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:login_signup/colorUtil.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.green, Colors.greenAccent])),
+      ),
+    );
+  }
+}
